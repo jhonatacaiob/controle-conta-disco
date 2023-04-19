75b35b84696276e8be7ad4452beb02bc36fd1829
jhonatacaiob <jhonatacaiob@gmail.com>
2023-04-18 22:00:28 -0300

    Implementa funcao para obter valor de porcentagem

--- controle/conversao.py
+++ controle/conversao.py
@@ -1,4 +1,7 @@
-from typing import SupportsInt
+from typing import (
+    SupportsInt,
+    SupportsFloat,
+)
 
 
 def converter_bytes_para_megabytes(quantidade_bytes: SupportsInt) -> float:
@@ -16,3 +19,20 @@ def converter_bytes_para_megabytes(quantidade_bytes: SupportsInt) -> float:
         return round(quantidade_mega, 2)
     except ValueError:
         raise ValueError('Valor não conversível para inteiro')
+
+
+def obtem_valor_da_porcentagem(parcial: SupportsFloat, total: SupportsFloat) -> str:
+    """Obtem o valor da porcentagem, e retorna formatado como string em duas casas decimais
+    >>> obtem_valor_da_porcentagem(5, 10)
+    '50.00%'
+    >>> obtem_valor_da_porcentagem(2, 4)
+    '50.00%'
+    >>> obtem_valor_da_porcentagem(8, 10)
+    '80.00%'
+    >>> obtem_valor_da_porcentagem(25, 10000)
+    '0.25%'
+    """
+    try:
+        return '{:.2%}'.format(float(parcial) / float(total))
+    except ValueError:
+        raise ValueError("Valor não conversível para float")
--- test/test_conversao.py
+++ test/test_conversao.py
@@ -1,27 +1,59 @@
 from unittest import TestCase
 
-from controle.conversao import converter_bytes_para_megabytes
+from controle.conversao import (
+    converter_bytes_para_megabytes,
+    obtem_valor_da_porcentagem,
+)
 
 
 class ConversaoTest(TestCase):
     def test_realiza_conversao(self):
         entrada = 1024 * 1024
-        self.assertEqual(1, converter_bytes_para_megabytes(entrada))
+        saida_esperada = 1
+        self.assertEqual(saida_esperada, converter_bytes_para_megabytes(entrada))
 
     def test_deve_realizar_conversao_ao_passar_um_float(self):
         entrada = 1024.0 * 1024
-        self.assertEqual(1, converter_bytes_para_megabytes(entrada))
+        saida_esperada = 1
+        self.assertEqual(saida_esperada, converter_bytes_para_megabytes(entrada))
 
     def test_deve_realizar_conversao_ao_passar_uma_string_conversivel(self):
         entrada = 2 * 1024 * 1024
-        self.assertEqual(2, converter_bytes_para_megabytes(str(entrada)))
+        saida_esperada = 2
+        self.assertEqual(saida_esperada, converter_bytes_para_megabytes(str(entrada)))
 
     def test_deve_realizar_conversao_e_retornar_um_valor_arredondado(self):
         entrada = 786432
-        self.assertEqual(0.75, converter_bytes_para_megabytes(entrada))
+        saida_esperada = 0.75
+        self.assertEqual(saida_esperada, converter_bytes_para_megabytes(entrada))
 
     def test_deve_retornar_um_erro_ao_passar_um_valor_não_conversivel(self):
         entrada = 'uma_string'
 
         with self.assertRaisesRegex(ValueError, 'Valor não conversível para inteiro'):
             converter_bytes_para_megabytes(entrada)
+
+    def test_aplica_porcentagem(self):
+        entrada = (2, 4)
+        saida_esperada = '50.00%'
+        self.assertEqual(saida_esperada, obtem_valor_da_porcentagem(*entrada))
+
+    def test_deve_aplicar_porcentagem_quando_primeiro_for_conversivel(self):
+        entrada = ('2.0', 4)
+        saida_esperada = '50.00%'
+        self.assertEqual(saida_esperada, obtem_valor_da_porcentagem(*entrada))
+
+    def test_deve_aplicar_porcentagem_quando_segundo_for_conversivel(self):
+        entrada = (3, '12.0')
+        saida_esperada = '25.00%'
+        self.assertEqual(saida_esperada, obtem_valor_da_porcentagem(*entrada))
+
+    def test_deve_retornar_erro_quando_algum_valor_não_é_conversivel(self):
+        entradas = [(3, 'porta'), ('maca', 4), ('uma_string', 'duas_string')]
+
+        for primeiro, segundo in entradas: 
+            with (
+                self.subTest(primeiro=primeiro, segundo=segundo),
+                self.assertRaisesRegex(ValueError, 'Valor não conversível para float')
+            ):
+                obtem_valor_da_porcentagem(primeiro, segundo)
